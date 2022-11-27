01e5382d33b4ccf1d21a063a9da66b7dc25e75ce
Prabhaharan1392 <115686911+Prabhaharan1392@users.noreply.github.com>
2022-11-27 22:56:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,43 @@
+let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
-let countryInp = document.getElementById("country-inp");
-searchBtn.addEventListener("click", () => {
-   let countryName = countryInp.value;
-   let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-   console.log(finalURL);
-   fetch(finalURL).then((response) => response.json()).then((data) => {
-   console.log(data[0]);
-   console.log(data[0].capital[0]);
-   console.log(data[0].flags.svg);
-   console.log(data[0].name.common);
-   console.log(data[0].region);
-   console.log(data[0].latlng);
-   console.log(data[0].cca3);
-   console.log(data[0].continents[0]);
-   console.log(Object.keys(data[0].currencies)[0]);
-   console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-   console.log(Object.values(data[0].languages).toString().split(",").join(","));
-   result.innerHTML = `<img src="${data[0].flags.svg}" class="flag-img">
-   <h2>${data[0].name.common}</h2>
-   <div class="wrapper">
-   <div class="data-wrapper">
-   <h4>Capital:</h4>
-   <span>${data[0].capital[0]}</span>
-   </div>
-   </div>
-   <div class="wrapper">
-   <div class="data-wrapper">
-   <h4>Region:</h4>
-   <span>${data[0].region}</span>
-   </div>
-   </div>
-   <div class="wrapper">
-   <div class="data-wrapper">
-   <h4>latlng:</h4>
-   <span>${data[0].latlng}</span>
-   </div>
-   </div>
-   <div class="wrapper">
-   <div class="data-wrapper">
-   <h4>Country code:</h4>
-   <span>${data[0].cca3}</span>
-   </div>
-   </div>
-   <div class="wrapper">
-   <div class="data-wrapper">
-   <h4>Currency:</h4>
-   <span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</span>
-   </div>
-   </div>
-   <div class="wrapper">
-   <div class="data-wrapper">
-   <h4>Common Languages:</h4>
-   <span>${Object.values(data[0].languages).toString().split(",").join(",")}</span>
-   </div>
-   </div>
-   `;
-});
-});
+let cityRef = document.getElementById("city");
+
+let getWeather = () => {
+    let cityValue = cityRef.value;
+    if(cityValue.length == 0){
+        result.innerHTML=`<h3>please enter a city name</h3>`;
+    }
+    else{
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+        fetch(url)
+        .then((resp) => resp.json())
+        .then(data => {
+            console.log(data);
+            console.log(data.weather[0].icon);
+            console.log(data.weather[0].main);
+            console.log(data.weather[0].description);
+            console.log(data.name);
+            console.log(data.main.temp_min);
+            console.log(data.main.temp_max);
+            result.innerHTML = `<h2>${data.name}</h2>
+            <h4 class="weather">${data.weather[0].description}</h4>
+            <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+            <h1>${data.main.temp} &#176;</h1> 
+            <div class="temp-container">
+            <div>
+            <h4 class="title">min</h4>
+            <h4 class="temp">${data.main.temp_min}</h4>
+            </div>
+            <div>
+            <h4 class="title">max</h4>
+            <h4 class="temp">${data.main.temp_max}</h4>
+            </div>
+            </div>
+            `;
+        })
+            .catch(() => {result.innerHTML = `<h3>City not found</h3>`;
+        });
+    }    
+};
+searchBtn.addEventListener("click", getWeather);
+window.addEventListener("load", getWeather);
